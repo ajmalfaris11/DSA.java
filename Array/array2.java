8a750ce749bc05b01cd9b9c810aa764d6d2e34d4
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-07-13 14:41:03 +0530

    array2 problem solved

--- Array/array2.java
+++ Array/array2.java
@@ -4,9 +4,9 @@
  * 
  * 
  * array-
- *   +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
- *   |  6  |  1  |  6  |  8  |  8  | 10  |  4  | 15  |  6  |  3  |  9  |  6  |  
- *   +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+ *   +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+ *   |  6  |  1  |  6  |  8  | 10  |  4  | 15  |  6  |  3  |  9  |  6  |  
+ *   +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
  * 
  * target = 6
  * 
@@ -16,6 +16,31 @@
 public class array2 {
 
     public static void main(String[] args) {
+        int[] arr = { 6, 1, 6, 8, 10, 4, 15, 6, 3, 9, 6 };
+        int target = 6;
 
+        targetToEnd(arr, target);
+        System.out.println("\nThe answer :-");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void targetToEnd(int[] arr, int target) {
+        int j = arr.length - 1;
+
+        for (int i = 0; i < j; i++) {
+            while (j > i && arr[j] == target) {
+                j--;
+            }
+
+            if (arr[i] == target) {
+                // Swap the elements
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
     }
 }
