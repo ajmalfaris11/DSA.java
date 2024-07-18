c7604f15a21b280d7f6265cb84b4645c941f76e6
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-07-18 18:56:16 +0530

    printing view changed and changed while loop condition head to temp

--- LinkedList/singly-linked-list/SLinkedList.java
+++ LinkedList/singly-linked-list/SLinkedList.java
@@ -12,51 +12,54 @@
 public class SLinkedList {
     class Node { // it'snot predefined class
 
-        int data;  //INT type
-        Node next; //NODE type
-        
-        Node(int data){
+        int data; // INT type
+        Node next; // NODE type
+
+        Node(int data) {
             this.data = data;
         }
     }
-    
+
     public Node head = null;
     public Node tail = null;
-    
-    public void addNode(int data){
+
+    public void addNode(int data) {
         Node newNode = new Node(data);
 
-        if( head == null ){
+        if (head == null) {
             head = newNode;
         } else {
             tail.next = newNode;
         }
-        tail = newNode ;
+        tail = newNode;
     }
 
-public void display(){
-    if (head == null){
-        System.out.println("list is empty");
-        return;
+    public void display(String name) {
+        System.out.println(String.format("\n\n%s node values\n-----------------", name));
+        if (head == null) {
+            System.out.println("list is empty");
+            return;
+        }
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.printf("%d, ", temp.data);
+            temp = temp.next;
+        }
+        System.out.println("\n\n");
     }
 
-    Node temp = head;
-    while (head != null){
-        System.out.println(temp.data);
-        temp = temp.next;
+    public static void main(String[] args) {
+        SLinkedList list = new SLinkedList();
+
+        list.addNode(10);
+        list.addNode(20);
+        list.addNode(30);
+        list.addNode(40);
+        list.addNode(50);
+        list.addNode(60);
+
+        list.display("list");
     }
-}
-
-public static void main(String[] args) {
-    SLinkedList list = new SLinkedList();
-
-    list.addNode(10);
-    list.addNode(20);
-    list.addNode(30);
-    list.addNode(40);
-    list.addNode(50);
-
-    list.display();
-}
 
 }
