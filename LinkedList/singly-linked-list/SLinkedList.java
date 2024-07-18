1fc1413e4f39f02839c6e1155ee4d15edead61e4
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-07-18 17:44:35 +0530

    Node class created

--- LinkedList/singly-linked-list/SLinkedList.java
+++ LinkedList/singly-linked-list/SLinkedList.java
@@ -1,3 +1,23 @@
+/*   
+ 
+    +--------+
+    |  data  |
+    +--------+   ---------> this is a node
+    |  next  |
+    +--------+
+
+ 
+ */
+
 public class SLinkedList {
+    class Node { // it'snot predefined class
+
+        int data;  //INT type
+        Node next; //NODE type
+        
+        Node(int data){
+            this.data = data;
+        }
+    }
 
 }
