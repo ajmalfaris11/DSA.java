805dee2b313bcf7171ff4a91c1f174763aeef008
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-07-19 09:13:45 +0530

    Updating the tail Pointer and commented each statements

--- LinkedList/singly-linked-list/SLinkedList.java
+++ LinkedList/singly-linked-list/SLinkedList.java
@@ -10,7 +10,8 @@
  */
 
 public class SLinkedList {
-    
+
+    // Node
     class Node { // it'snot predefined class
 
         int data; // INT type
@@ -24,7 +25,7 @@ public class SLinkedList {
     public Node head = null;
     public Node tail = null;
 
-// Add Node to NodList 
+    // Add Node to NodList
     public void addNode(int data) {
         Node newNode = new Node(data);
 
@@ -36,6 +37,7 @@ public class SLinkedList {
         tail = newNode;
     }
 
+    // dispaly the Node List
     public void display(String name) {
         System.out.println(String.format("\n\n%s node values\n-----------------", name));
         if (head == null) {
@@ -43,17 +45,58 @@ public class SLinkedList {
             return;
         }
 
-        Node temp = head;
-        while (temp != null) {
+        Node temp = head; //head node stored to tamp for travers other ways head will change
+        while (temp != null) { //taversing the NodeList
             System.out.printf("%d, ", temp.data);
-            temp = temp.next;
+            temp = temp.next; 
         }
         System.out.println("\n\n");
     }
 
+    // deleting Node
+
+ public void delete(int data) {
+    Node temp = head, prev = null;
+    
+
+    // Check if the head node needs to be deleted
+    if (temp != null && temp.data == data) {
+        head = temp.next;
+        // If head was also the tail, update tail
+        if (head == null) {
+            tail = null;
+        }
+        return;
+    }
+
+    // Traverse the list to find the node to delete
+    while (temp != null && temp.data != data) {
+        prev = temp;
+        temp = temp.next;
+    }
+
+    // If the node to delete was not found
+    if (temp == null) {
+        return;
+    }
+
+    // Update the next pointer of the previous node to skip the node to be deleted
+    if (temp == tail) {
+        tail = prev;
+        if (tail != null) {
+            tail.next = null;
+        }
+    } else {
+        prev.next = temp.next;
+    }
+}
+
+
+    // main method
     public static void main(String[] args) {
         SLinkedList list = new SLinkedList();
 
+        // adding nod datas to Nod List
         list.addNode(10);
         list.addNode(20);
         list.addNode(30);
@@ -61,7 +104,11 @@ public class SLinkedList {
         list.addNode(50);
         list.addNode(60);
 
+        // call the display funciton to print nod values
+
+        list.delete(20);
         list.display("list");
+
     }
 
 }
